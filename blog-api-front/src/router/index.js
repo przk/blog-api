1b2a6e05a7a64ce4371993ec755cbd79e51619ce
przk <286289@stud.umk.pl>
2022-05-11 14:58:37 +0200

    repair lazy routes

--- blog-api-front/src/router/index.js
+++ blog-api-front/src/router/index.js
@@ -8,72 +8,72 @@ export default new Router({
     {
       path: '/',
       name: 'MainPage',
-      component: import('@/components/MainPage')
+      component: () => import('@/components/MainPage')
     },
     {
       path: '/page/:pageId',
       name: 'Page',
-      component: import('@/components/Page')
+      component: () => import('@/components/Page')
     },
     {
       path: '/newpage',
       name: 'newPage',
-      component: import('@/components/NewPage')
+      component: () => import('@/components/NewPage')
     },
     {
       path: '/editpage/:pageId',
       name: 'editPage',
-      component: import('@/components/EditPage')
+      component: () => import('@/components/EditPage')
     },
     {
       path: '/page/:pageId/comments/',
       name: 'Comments',
-      component: import('@/components/Comments')
+      component: () => import('@/components/Comments')
     },
     {
       path: '/signin',
       name: 'Signin',
-      component: import('@/components/Signin')
+      component: () => import('@/components/Signin')
     },
     {
       path: '/signup',
       name: 'Signup',
-      component: import('@/components/Signup')
+      component: () => import('@/components/Signup')
     },
     {
       path: '/404',
       name: 'ErrNotFound',
-      component: import('@/components/ErrNotFound')
+      component: () => import('@/components/ErrNotFound')
     },
     {
       path: '/panel',
       name: 'AdminPanel',
-      component: import('@/components/AdminPanel')
+      component: () => import('@/components/AdminPanel')
     },
     {
       path: '/panel/users',
       name: 'AdminPanelUsers',
-      component: import('@/components/AdminPanelUsers')
+      component: () => import('@/components/AdminPanelUsers')
     },
     {
       path: '/panel/pages',
       name: 'AdminPanelPages',
-      component: import('@/components/AdminPanelPages')
+      component: () => import('@/components/AdminPanelPages')
     },
     {
       path: '/panel/pages/:pageId',
       name: 'AdminPanelPage',
-      component: import('@/components/AdminPanelPage')
+      component: () => import('@/components/AdminPanelPage')
     },
     {
       path: '/panel/users/:userId',
       name: 'AdminPanelUser',
-      component: import('@/components/AdminPanelUser')
+      component: () => import('@/components/AdminPanelUser')
     },
     {
       path: '/edituser',
       name: 'EditUser',
-      component: import('@/components/EditUser')
+      component: () => import('@/components/EditUser')
     }
   ]
 })
